69b68f2022a15d9f7aa8a14e94423baa4a122150
John Moon <john.moon@livingsocial.com>
2014-05-11 09:02:53 -0400

    Initial Commit

---
+++ Workout/PJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PJAppDelegate.h
+//  Workout
+//
+//  Created by John Moon on 5/11/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Workout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Workout
+//
+//  Created by John Moon on 5/11/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PJAppDelegate class]));
+    }
+}
