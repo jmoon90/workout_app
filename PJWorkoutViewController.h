a74fa2dad94bebbbf5eaf689ccb1c46bb40ab5fd
John Moon <john.moon@livingsocial.com>
2014-05-11 09:04:55 -0400

    Initial setup Create PJWorkoutViewController

---
+++ PJWorkoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  PJWorkoutViewController.h
+//  Workout
+//
+//  Created by John Moon on 5/11/14.
+//  Copyright (c) 2014 John Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJWorkoutViewController : UIViewController
+
+@end
